fef3d15406c8a29cff471c05b26b0db8feaeb15c
Gerard Tolan <tolangerard@gmail.com>
2020-05-14 17:54:06 +0100

    remove Fb config

--- src/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyCIu6Lrv5jeb-aVUtF7tAdDp_yYz-mogx0",
-    authDomain: "vue-typescript-classes.firebaseapp.com",
-    databaseURL: "https://vue-typescript-classes.firebaseio.com",
-    projectId: "vue-typescript-classes",
-    storageBucket: "vue-typescript-classes.appspot.com",
-    messagingSenderId: "881612815436",
-    appId: "1:881612815436:web:c8215d7284b93f477088e9"
-};
